d2a4c53fbd8721e11c4915dce3e0618a1204143a
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-27 18:10:19 +0200

    ADD CategoryController

---
+++ net-il-mio-fotoalbum/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using net_il_mio_fotoalbum.Data;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CategoryController : Controller
+    {
+        [Route("/Admin/Categories")]
+        public IActionResult Index()
+        {
+            return View("/Views/Admin/Category/Index.cshtml");
+        }
+
+        [HttpGet]
+        [Route("/Admin/Categories/Create")]
+        public IActionResult Create()
+        {
+            Category category = new Category();
+            return View("/Views/Admin/Category/Create.cshtml", category);
+        }
+
+        [HttpPost]
+        [Route("/Admin/Categories/Create")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public IActionResult Create(Category data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("/Views/Admin/Category/Create.cshtml", data);
+            }
+
+            AdminManager.AddNewCategory(data);
+            return RedirectToAction("Index");
+        }
+    }
+}
