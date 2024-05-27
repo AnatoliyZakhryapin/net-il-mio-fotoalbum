c124809101c8a882c4b476672c13a88bceadddfc
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-27 16:00:02 +0200

    ADD Autorize to AdminController

--- net-il-mio-fotoalbum/Controllers/AdminController.cs
+++ net-il-mio-fotoalbum/Controllers/AdminController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace net_il_mio_fotoalbum.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         public IActionResult Index()
