a8a5689c3798f203842aee13f6030cda350c3e01
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-27 10:51:10 +0200

    ADD DBContext

---
+++ net-il-mio-fotoalbum/Data/FotoAlbumContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Data
+{
+    public class FotoAlbumContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Image> Images { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=FotoAlbum;Integrated Security=True;Pooling=False;Encrypt=False;TrustServerCertificate=False");
+        }
+    }
+}
