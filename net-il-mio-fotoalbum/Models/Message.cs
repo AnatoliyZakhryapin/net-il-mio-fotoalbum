7a23b4e2e36812dbaca6fe60cee07b58501e2af8
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-27 10:50:33 +0200

    ADD Class Message

---
+++ net-il-mio-fotoalbum/Models/Message.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    [Table("message")]
+    public class Message
+    {
+        public long MessageId { get; set; }
+        public string SenderName { get; set; }
+        public string SenderSurname { get; set; }
+        public string SenderEmail {  get; set; }
+        public string MessageText { get; set; }
+        public DateTime SendAt { get; set; }
+        public long UserID { get; set; }
+        public User User { get; set; }
+        public Message() { }
+    }
+}
