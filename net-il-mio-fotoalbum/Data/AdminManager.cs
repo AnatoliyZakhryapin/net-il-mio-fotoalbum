e4b456e91cb313be5f1f14a2b30f996cff305553
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-27 18:11:38 +0200

    ADD AdminManager

---
+++ net-il-mio-fotoalbum/Data/AdminManager.cs
@@ -0,0 +1,20 @@
+﻿using net_il_mio_fotoalbum.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace net_il_mio_fotoalbum.Data
+{
+    public static class AdminManager
+    {
+        public static bool AddNewCategory(Category category)
+        {
+            try
+            {
+                using FotoAlbumContext db = new FotoAlbumContext();
+                db.Add(category);
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+    }
+}
