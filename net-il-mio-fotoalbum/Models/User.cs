054ebaf04b095a003429fd66974018255a5b9cbb
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-27 10:50:27 +0200

    ADD Class User

---
+++ net-il-mio-fotoalbum/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    [Table("user")]
+    public class User
+    {
+        public long UserID {  get; set; }
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
+        public List<Image> Images { get; set; }
+        public List<Message> Messages { get; set; }
+        public User() { }
+    }
+}
