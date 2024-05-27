609bd4323a72b78ba20cefb5b59e994b5817f23e
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-27 10:50:43 +0200

    ADD Class Image

---
+++ net-il-mio-fotoalbum/Models/Image.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    [Table("image")]
+    public class Image
+    {
+        public long ImageId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url {  get; set; }
+        public bool IsVisibile { get; set; }
+        public bool HasPermitVisibility { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+        public List<Category> Categories { get; set; }
+        public long UserId { get; set; }
+        public User User { get; set; }
+    }
+}
