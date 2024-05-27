8d45fcd66eb22bb207cc964d55312b0603a8c1f5
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-27 10:50:52 +0200

    ADD Class Category

---
+++ net-il-mio-fotoalbum/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    [Table("category")]
+    [Index(nameof(Name), IsUnique = true)]
+    public class Category
+    {
+        public long CategoryId {  get; set; }
+        [Column("category_name")]
+        public string Name { get; set; }
+        public List<Image> Images { get; set; }
+        public Category() { }
+    }
+}
