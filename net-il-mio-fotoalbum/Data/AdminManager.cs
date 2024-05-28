4a8bb4bf0809705733b684c8c7c9274d9ce63be0
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-28 08:01:25 +0200

    ADD functions to AdminManager

--- net-il-mio-fotoalbum/Data/AdminManager.cs
+++ net-il-mio-fotoalbum/Data/AdminManager.cs
@@ -16,5 +16,65 @@ namespace net_il_mio_fotoalbum.Data
             }
             catch { return false; }
         }
+
+        public static bool AddNewImage(Image image, List<string> selectedCategories) {
+            try
+            {
+                using FotoAlbumContext db = new FotoAlbumContext();
+
+                if(selectedCategories != null)
+                {
+                    image.Categories = new List<Category>();
+                    foreach (var item in selectedCategories)
+                    {
+                        long categoryId = long.Parse(item);
+                        Category category = db.Categories.FirstOrDefault(c => c.CategoryId == categoryId);
+                        image.Categories.Add(category);
+                    }
+                }
+
+                db.Images.Add(image);
+                db.SaveChanges ();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public static Profile GetProfileByUserId(string id)
+        {
+            using FotoAlbumContext db = new FotoAlbumContext();
+            return db.Profiles.FirstOrDefault(p => p.UserId == id);
+        }
+        public static List<Category> GetAllCategories(bool includeReferences = false)
+        {
+            using FotoAlbumContext db = new FotoAlbumContext();
+            if (includeReferences)
+                return db.Categories.Include(c => c.Images).ToList();
+            return db.Categories.ToList();
+        }
+        public static List<Image> GetAllImages(bool includeReferences = false)
+        {
+            using FotoAlbumContext db = new FotoAlbumContext();
+
+            if(includeReferences)
+                return db.Images.Include(i => i.Categories).Include(i => i.Profile).ToList();
+            return db.Images.ToList();
+        }
+
+        public static FormModel CreateFormModel(Image image = null)
+        {
+            FormModel formModel = new FormModel();
+
+            if(image != null)
+            {
+                formModel.Image = image;
+                formModel.CreateCategories();
+            }
+
+            formModel.Image = new Image ();
+            formModel.CreateCategories();
+
+            return formModel;
+        }
     }
 }
