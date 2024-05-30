822937301a3056fe5bb8261276a3b40053d508bb
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-30 11:52:30 +0200

    REFACTOR Profile - Add nullable to fields

--- net-il-mio-fotoalbum/Models/Profile.cs
+++ net-il-mio-fotoalbum/Models/Profile.cs
@@ -8,10 +8,10 @@ namespace net_il_mio_fotoalbum.Models
         public long ProfileId {  get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public List<Image> Images { get; set; }
-        public List<Message> Messages { get; set; }
-        public string UserId { get; set; }
-        public User User { get; set; }
+        public List<Image>? Images { get; set; }
+        public List<Message>? Messages { get; set; }
+        public string? UserId { get; set; }
+        public User? User { get; set; }
         public Profile() { }
 
         public string GetFullName()
